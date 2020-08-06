ffc86b0c95c9e70534530c8930169cc2385e8466
Aiden Burgess <aiden9874@gmail.com>
2020-08-07 11:53:48 +1200

    create commandparser package and template classes

---
+++ project1/src/main/java/commandparser/Command.java
@@ -0,0 +1,10 @@
+package main.java.commandparser;
+
+public class Command {
+    public int numProcessors;
+    String inputFileName;
+    boolean isParallelised;
+    int numParallelCores;
+    boolean hasVirtualisation;
+    String outputFileName;
+}
---
+++ project1/src/main/java/commandparser/CommandParser.java
@@ -0,0 +1,9 @@
+package main.java.commandparser;
+
+public class CommandParser {
+    public static Command parse(String[] args) {
+        System.out.println("Command has been parsed!");
+        return new Command();
+    }
+
+}
