7caa2249e4efe7b856b50f91f8b1be800421f945
Aiden Burgess <aiden9874@gmail.com>
2020-08-07 19:01:05 +1200

    Add basic JUnit4 test and library

---
+++ project1/src/tests/java/DriverTest.java
@@ -0,0 +1,12 @@
+package tests.java;
+
+public class DriverTest {
+
+    @org.junit.Test
+    public void main() {
+    }
+
+    @org.junit.Test
+    public void startVisualisationThread() {
+    }
+}
