f96ae2a7f5c69fdda55b43c8b8bf20fde3e18714
Aiden Burgess <aiden9874@gmail.com>
2020-08-07 18:14:35 +1200

    Create flow for program

--- project1/src/main/java/Driver.java
+++ project1/src/main/java/Driver.java
@@ -1,7 +1,38 @@
 package main.java;
 
+import main.java.commandparser.Command;
+import main.java.commandparser.CommandParser;
+import main.java.dotio.DotIO;
+import main.java.dotio.TaskGraph;
+import main.java.scheduler.BaseScheduler;
+import main.java.scheduler.Scheduler;
+
+import java.util.HashMap;
+
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Command command = CommandParser.parse(args);
+        TaskGraph taskGraph = DotIO.read(command.inputFileName);
+
+        Scheduler scheduler = new BaseScheduler(taskGraph);
+
+        // Uncomment this to force visualisation on
+        command.hasVisualisation = true;
+        if (command.hasVisualisation) {
+            startVisualisationThread(scheduler);
+        }
+
+        scheduler.execute();
+
+        HashMap<String, Integer> startTimeMap = scheduler.getStartTimeMap();
+        HashMap<String, Integer> processorMap = scheduler.getProcessorMap();
+
+        DotIO.write(command.outputFileName, taskGraph, startTimeMap, processorMap);
+    }
+
+    public static void startVisualisationThread(Scheduler scheduler) {
+        new Thread(() -> {
+            FXController.main(scheduler);
+        }).start();
     }
 }
