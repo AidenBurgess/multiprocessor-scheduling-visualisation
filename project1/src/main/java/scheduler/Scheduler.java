fb084664c5d3a4515d7ff646fda6823886f939fd
Aiden Burgess <aiden9874@gmail.com>
2020-08-07 12:02:02 +1200

    Start adding basic method descriptions

--- project1/src/main/java/scheduler/Scheduler.java
+++ project1/src/main/java/scheduler/Scheduler.java
@@ -1,5 +1,23 @@
 package main.java.scheduler;
 
+import main.java.dotio.Task;
+import main.java.dotio.TaskGraph;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class Scheduler {
+    TaskGraph input;
+    int numProcessors;
+    Schedule currentState;
+    Schedule bestState;
+    ThreadPoolExecutor threadPool;
+
+    public Scheduler(TaskGraph taskGraph) {
+        System.out.println("Scheduler created!");
+    }
+
+    public void execute() {
+        System.out.println("Execute called!");
+    }
 
 }
