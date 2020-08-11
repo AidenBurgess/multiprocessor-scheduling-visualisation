2e447ec7246f4f9ce5b3de90a0d1f627e42d0d55
Sreeniketh Raghavan <sreeniketh0609@gmail.com>
2020-08-11 17:33:13 +1200

    Created the Processor and ProcessorManager classes

---
+++ project1/src/main/java/scheduler/Processor.java
@@ -0,0 +1,12 @@
+package main.java.scheduler;
+
+import java.util.ArrayList;
+
+public class Processor {
+
+    public List<Task> tasks;
+
+    public Processor() {
+        tasks = new ArrayList<>();
+    }
+}
---
+++ project1/src/main/java/scheduler/ProcessorManager.java
@@ -0,0 +1,12 @@
+package main.java.scheduler;
+
+import java.util.List;
+
+public class ProcessorManager {
+
+    List<Processor> processors;
+
+    public ProcessorManager(List<Processor> processors) {
+        this.processors = processors;
+    }
+}
