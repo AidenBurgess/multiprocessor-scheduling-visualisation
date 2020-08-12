42a24878235a7d006a77bd04cd091fa2d73a6131
Sreeniketh Raghavan <sreeniketh0609@gmail.com>
2020-08-12 17:15:06 +1200

    Created an Edge class to store the communication delays
    Pair programming with Anubhav

--- project1/src/main/java/scheduler/Edge.java
+++ project1/src/main/java/scheduler/Edge.java
@@ -1,4 +1,23 @@
 package main.java.scheduler;
 
 public class Edge {
+
+    private Node from;
+    private int weight;
+
+
+    public Edge(Node from, int weight) {
+        this.from = from;
+        this.weight = weight;
+    }
+
+    public Node getFrom() {
+        return from;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+
 }
