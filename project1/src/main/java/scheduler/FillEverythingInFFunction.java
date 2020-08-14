d44265253aa1ce8044fa44d3c699e4222a6eb3f6
sammochen <samuelshchen@gmail.com>
2020-08-15 10:40:34 +1200

    Commented TopologicalFFunction

--- project1/src/main/java/scheduler/FillEverythingInFFunction.java
+++ project1/src/main/java/scheduler/FillEverythingInFFunction.java
@@ -3,35 +3,30 @@ package main.java.scheduler;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * The strategy for this FFunction is to see how much total time there is left, and pack the times in as tight
+ * as possible onto the current Schedule. E.g.
+ *
+ * Schedule:
+ * P1: XXX
+ * P2: XXXXXXX
+ * P3: X
+ *
+ * Unscheduled task weights: 4, 5, 3
+ * The total time is worked out, 12, and 'stuffed' into the schedule.
+ *
+ * Estimated Schedule:
+ * P1: XXX-----
+ * P2: XXXXXXX-
+ * P3: X------
+ *
+ * Notice that after 'stuffing' 12 units of time in, the best endTime that the Schedule can have is 8.
+ * Also notice that when 'stuffing' the original task times are not necessarily together (this is a computationally
+ * easier problem).
+ *
+ * This evaluate method, for this case, will return 8.
+ */
 public class FillEverythingInFFunction implements FFunction {
-    /**
-     * The strategy for this FFunction is to see how much total time there is left, and pack the times in as tight
-     * as possible onto the current Schedule. E.g.
-     *
-     * Schedule:
-     * P1: XXX
-     * P2: XXXXXXX
-     * P3: X
-     *
-     * Unscheduled task weights: 4, 5, 3
-     * The total time is worked out, 12, and 'stuffed' into the schedule.
-     *
-     * Estimated Schedule:
-     * P1: XXX-----
-     * P2: XXXXXXX-
-     * P3: X------
-     *
-     * Notice that after 'stuffing' 12 units of time in, the best endTime that the Schedule can have is 8.
-     * Also notice that when 'stuffing' the original task times are not necessarily together (this is a computationally
-     * easier problem).
-     *
-     * This evaluate method, for this case, will return 8.
-     *
-     * @param s Schedule
-     * @param nodeMap Nodes
-     * @param incomingEdgesMap Dependencies
-     * @return The minimum time that the Schedule will require to complete.
-     */
     @Override
     public int evaluate(Schedule s, HashMap<String, TaskNode> nodeMap, HashMap<TaskNode, List<Edge>> incomingEdgesMap) {
         // First, calculate the total time left of the unscheduled tasks.
