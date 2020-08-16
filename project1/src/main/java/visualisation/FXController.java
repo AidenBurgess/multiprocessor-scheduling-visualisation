a0f86dd0790b3265428cfce42f61bd5fd94395fc
Aiden Burgess <aiden9874@gmail.com>
2020-08-17 09:58:36 +1200

    Add support for jfoenix and scenebuilder

--- project1/src/main/java/visualisation/FXController.java
+++ project1/src/main/java/visualisation/FXController.java
@@ -1,15 +1,13 @@
 package main.java.visualisation;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import main.java.scheduler.Scheduler;
+import java.io.IOException;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  *
@@ -25,28 +23,10 @@ public class FXController extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        // Setup polling the scheduler
-        Timer t = new Timer();
-        t.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                onRefresh();
-            }
-        }, 1000, 1000);
-
-        primaryStage.setTitle("Hello World!");
-
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction((ActionEvent event) -> {
-                System.out.println("Our scheduler has " + FXController.sc.getNumProcessors() + " processors!");
-                System.out.println("Hello World!");
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+        primaryStage.setTitle("Visualisation");
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
