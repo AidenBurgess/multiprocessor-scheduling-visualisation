090371e333bf5a1450b8e7206583b009e597b4c6
Aiden Burgess <aiden9874@gmail.com>
2020-08-18 14:31:36 +1200

    Create SystemPerformanceRetriever, getCPUUsagePercent() and getRAMUsageBytes()

---
+++ project1/src/main/java/dataretriever/SystemPerformanceRetriever.java
@@ -0,0 +1,24 @@
+package main.java.dataretriever;
+
+import java.lang.management.ManagementFactory;
+import com.sun.management.OperatingSystemMXBean;
+
+public class SystemPerformanceRetriever {
+    private final OperatingSystemMXBean statsRetriever = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+    private final long totalRAM = statsRetriever.getTotalPhysicalMemorySize();
+
+    /**
+     * @return double representing the CPU percentage use
+     */
+    public double getCPUUsagePercent() {
+        return statsRetriever.getProcessCpuLoad() * 100;
+    }
+
+    /**
+     * @return long representing the RAM usage in bytes
+     */
+    public long getRAMUsageBytes() {
+        return totalRAM - statsRetriever.getFreePhysicalMemorySize();
+    }
+
+}
