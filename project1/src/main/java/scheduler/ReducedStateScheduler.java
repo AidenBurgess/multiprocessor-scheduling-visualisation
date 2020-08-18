1f2d09b6a5fa244287e91911fa49522235a2ba1a
sammochen <samuelshchen@gmail.com>
2020-08-19 11:42:35 +1200

    Refactored State to be in its own file

--- project1/src/main/java/scheduler/ReducedStateScheduler.java
+++ project1/src/main/java/scheduler/ReducedStateScheduler.java
@@ -184,7 +184,7 @@ public class ReducedStateScheduler implements Scheduler {
                 // For each processor,
                 for (int processor = 0; processor < p; processor++) {
                     // Prune
-                    if (bound != NO_SOLUTION && state.endTime + 1 >= bound) return;
+                    if (bound != NO_SOLUTION && state.endTime >= bound) return;
 
                     // Find the earliest time that task can be placed on processor.
                     // For each of its dependencies, make sure that there is enough delay.
@@ -227,51 +227,3 @@ public class ReducedStateScheduler implements Scheduler {
     }
 }
 
-/**
- * The State class holds information that defines an allocation of tasks on processors.
- */
-class State {
-    public static final int UNSCHEDULED = -1;
-    private final int n, p;
-
-
-    protected int[] assignedProcessorId; // assignedProcessorId[taskId] -> processorId
-    protected int[] taskEndTime; // taskEndTime[taskId] -> end time of task
-    protected int[] processorEndTime; // processorEndTime[processorId] -> end time of processor
-
-    protected int endTime; // end time of the last processor
-    protected int unassignedTasks; // number of tasks still unassigned
-
-    public State(int n, int p) {
-        this.n = n;
-        this.p = p;
-
-        unassignedTasks = n; // initially n tasks are unassigned
-        endTime = 0;
-
-        processorEndTime = new int[p];
-        assignedProcessorId = new int[n];
-        taskEndTime = new int[n];
-
-        for (int i = 0; i < n; i++) assignedProcessorId[i] = UNSCHEDULED;
-    }
-
-    /**
-     * Returns a deep copy of the current State
-     * @return a new State instance with the same values.
-     */
-    public State copy() {
-        State next = new State(n, p);
-        for (int i = 0; i < n; i++) {
-            next.assignedProcessorId[i] = assignedProcessorId[i];
-            next.taskEndTime[i] = taskEndTime[i];
-        }
-        for (int i = 0; i < p; i++) {
-            next.processorEndTime[i] = processorEndTime[i];
-        }
-
-        next.endTime = endTime;
-        next.unassignedTasks = unassignedTasks;
-        return next;
-    }
-}
