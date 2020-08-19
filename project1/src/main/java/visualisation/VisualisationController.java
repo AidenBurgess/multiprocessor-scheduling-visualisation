40ab5fd5998d73fad012fea696869dadab9cff59
AnubhavKhanna98 <anubhavkhanna.nz@gmail.com>
2020-08-20 07:53:44 +1200

    Fixed broken code after merging master into this branch

--- project1/src/main/java/visualisation/VisualisationController.java
+++ project1/src/main/java/visualisation/VisualisationController.java
@@ -26,39 +26,15 @@ import main.java.dataretriever.SystemPerformanceRetriever;
 import main.java.scheduler.Scheduler;
 import main.java.visualisation.ScheduleChart.ExtraData;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
 
 //todo Ask the group if we want the GanttChart class named to be
 // changed or we want to retain it. Also whether we want the fields
 // in the GanttChart class should start with underscore
-
-public class VisualisationController {
-
-    // FXML Fields
-    @FXML
-    private VBox _bestScheduleParent;
-    @FXML
-    private VBox _currentScheduleParent;
-
-    // Non-FXML Fields
-    private ScheduleChart<Number, String> _currentScheduleChart;
-    private ScheduleChart<Number, String> _bestScheduleChart;
-    private Scheduler _scheduler;
-    private List<Task> _taskList;
-    private int _numProcessors;
-
-    @FXML
-    void initialize() {
-        _scheduler = VisualisationDriver.getScheduler();
-        _taskList = VisualisationDriver.getTaskGraph().getTasks();
-        _numProcessors = VisualisationDriver.getNumProcessors();
-import java.net.URL;
-import java.util.ResourceBundle;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class VisualisationController implements Initializable {
 
@@ -74,6 +50,7 @@ public class VisualisationController implements Initializable {
     private int _seconds;
     private int _milliseconds;
 
+    // FXML Fields
     @FXML
     private VBox currentSchedule;
 
@@ -104,8 +81,21 @@ public class VisualisationController implements Initializable {
     @FXML
     private Text completedSchedulesFigure;
 
+    @FXML
+    private VBox _bestScheduleParent;
+
+    @FXML
+    private VBox _currentScheduleParent;
+
+    // Non-FXML Fields
+    private ScheduleChart<Number, String> _currentScheduleChart;
+    private ScheduleChart<Number, String> _bestScheduleChart;
+    private List<Task> _taskList;
+    private int _numProcessors;
+
     /**
      * Updates the refresh rate, yet to be implemented
+     *
      * @param refreshRate
      */
     private void updateRefreshRate(int refreshRate) {
@@ -198,8 +188,10 @@ public class VisualisationController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        _sc = VisualisationDriver.sc;
+        _sc = VisualisationDriver.getScheduler();
         _performanceRetriever = new SystemPerformanceRetriever();
+        _taskList = VisualisationDriver.getTaskGraph().getTasks();
+        _numProcessors = VisualisationDriver.getNumProcessors();
 
         // initialise the time to 0.00
         _seconds = 0;
@@ -219,17 +211,28 @@ public class VisualisationController implements Initializable {
 
             @Override
             public void run() {
-                refreshStats();
+                updateStatistics();
+
+                // queue tasks on the other thread
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+//                        CPUSeries.getData().add(new XYChart.Data<>(Integer.toString(i++),10));
+                        addCPUChartData();
+                        addRAMChartData();
+                    }
+                });
             }
-        }, 1000, 1000);
+
+        }, _refreshRate, 1000);
         chartSetup();
     }
 
     private void chartSetup() {
         // Setting up the y-axis
         List<String> processorsList = new ArrayList<String>();
-        for (int i=0; i < _numProcessors; i++) {
-            processorsList.add("Processor "+i+1);
+        for (int i = 0; i < _numProcessors; i++) {
+            processorsList.add("Processor " + i + 1);
         }
         CategoryAxis yAxis = new CategoryAxis();
         yAxis.setCategories(FXCollections.observableArrayList(processorsList));
@@ -258,34 +261,20 @@ public class VisualisationController implements Initializable {
         // Run through each task, create an XYChart.Data object and put
         // this object in the Series object which corresponds to the processor this task is scheduled on
         for (Task task : _taskList) {
-            int processorOfTask = _scheduler.getProcessorMap().get(task.getName());
-            int taskStartTime = _scheduler.getStartTimeMap().get(task.getName());
+            int processorOfTask = bestProcessorMap.get(task.getName());
+            int taskStartTime = bestProcessorMap.get(task.getName());
             int taskTime = task.getTaskTime();
 
-            XYChart.Data taskData = new XYChart.Data(taskStartTime, "Processor "+processorOfTask, new ExtraData(taskTime));
+            XYChart.Data taskData = new XYChart.Data(taskStartTime, "Processor " + processorOfTask, new ExtraData(taskTime));
 
             // -1 has been used below because the seriesArray is 0 indexed whereas the processor numbers are 1 indexed
-            seriesArray[processorOfTask-1].getData().add(taskData);
+            seriesArrayBest[processorOfTask - 1].getData().add(taskData);
         }
 
         // Put the Series objects generated above in the chart after clearing the chart's existing data
         _bestScheduleChart.getData().clear();
-        for (Series series : seriesArray) {
+        for (Series series : seriesArrayBest) {
             _bestScheduleChart.getData().add(series);
         }
-    }
-                updateStatistics();
-
-                // queue tasks on the other thread
-                Platform.runLater(new Runnable() {
-                    @Override public void run() {
-//                        CPUSeries.getData().add(new XYChart.Data<>(Integer.toString(i++),10));
-                        addCPUChartData();
-                        addRAMChartData();
-                    }
-                });
-            }
-
-        }, _refreshRate, 1000);
     }
 }
