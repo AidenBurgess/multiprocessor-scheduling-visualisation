7fd55ea2aeb9548d1b0dd47c02385aaac476dcb9
Seif Younes <seif99y@gmail.com>
2020-08-21 15:33:03 +1200

    Rewrote comments for DotIO.read() and deleted ReadGraphObject as it is no longer needed

--- project1/src/main/java/dotio/DotIO.java
+++ project1/src/main/java/dotio/DotIO.java
@@ -14,8 +14,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Read a string of the file name and extract the contents to return a task graph.
- *
+ * This static class handles the reading and writing of .dot syntax. It handles all the reading and writing of files,
+ * as well as any syntax or error checking when it comes to the actual .dot file format.
  */
 public class DotIO {
 
@@ -35,7 +35,9 @@ public class DotIO {
      * }
      *
      * @param inputFile The name of the file to be read.
-     * @return
+     * @return The TaskGraph object
+     * @throws DotIOException If there is an issue with reading the file, or an issue with the .dot syntax of the file.
+     * @throws FileNotFoundException If the given file is... not found...
      */
     public static TaskGraph read(String inputFile) throws DotIOException, FileNotFoundException {
         TaskGraph graph = new TaskGraph();
@@ -45,90 +47,10 @@ public class DotIO {
             DOTParser parser = new DOTParser(new CommonTokenStream(lexer));
             ParseTree tree = parser.graph();
             ParseTreeWalker.DEFAULT.walk(new AdaptedDotListener(graph), tree);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            throw new DotIOException("Java IO error when reading input file.");
         }
         return graph;
-    }
-
-    /**
-     * Read an individual graph object (i.e. a node or an edge) from a dot file and add it to the specified graph object
-     * @param tk The StreamTokenizer object being used to parse the input file.
-     * @param graph The TaskGraph object being created to encapsulate the graph from the .dot file.
-     * @throws IOException If an IOException is thrown by the StreamTokenizer object.
-     * @throws DotIOException If the .dot syntax is invalid or incorrect.
-     */
-    private static void readGraphObject(StreamTokenizer tk, TaskGraph graph) throws IOException, DotIOException {
-
-        String srcNode = null;
-        String destNode = null;
-        int weight = 0;
-
-        //Check for first word: the name of the node, or the name of the source node of the edge
-        if (tk.ttype == StreamTokenizer.TT_WORD) {
-            srcNode = tk.sval;
-            tk.nextToken();
-        } else {
-            throw new DotIOException("First token in the line wasn't a node name"); //Error: First token in the line wasn't a node name
-        }
-
-        //Check if the element is an edge by checking for the '->' sequence. If it is, then parse the dest node.
-        //Note: also checks for different type of hyphen character: as we're not sure if input is '−' or '-'
-        if ((tk.ttype == '-') || ((tk.ttype == StreamTokenizer.TT_WORD) && tk.sval.equals("−"))) {
-            tk.nextToken();
-            if (tk.ttype == '>') {
-                tk.nextToken();
-            } else {
-                throw new DotIOException("Found other character when expecting '>'");
-            }
-            if (tk.ttype == StreamTokenizer.TT_WORD) {
-                destNode = tk.sval;
-                tk.nextToken();
-            } else {
-                throw new DotIOException("destination of edge is not a word"); //Error: destination of edge is not a word"
-            }
-        }
-
-        //Check for the '[' symbol before the weight property.
-        if (tk.ttype == '[') {
-            tk.nextToken();
-        } else {
-            throw new DotIOException("Found other character when expecting '['"); //Error: found other character when expecting '['
-        }
-
-        //Check that the weight of the node is correctly notated.
-        if ((tk.ttype == StreamTokenizer.TT_WORD) && tk.sval.equalsIgnoreCase("Weight")) {
-            tk.nextToken();
-        } else {
-            throw new DotIOException("Weight of node/edge not specified"); //Error: Weight of node/edge not specified.
-        }
-
-        if (tk.ttype == '=') {
-            tk.nextToken();
-        } else {
-            throw new DotIOException("Found other character when expecting '='");
-        }
-
-        if (tk.ttype == StreamTokenizer.TT_NUMBER) {
-            weight = (int) tk.nval;
-            tk.nextToken();
-        } else {
-            throw new DotIOException("Weight value for node is not a number.");
-        }
-
-        //Check that there is a ']' character after weight property.
-        if (tk.ttype == ']') {
-            tk.nextToken();
-        } else {
-            throw new DotIOException("Couldn't find ']' character"); //Error: Couldn't find ']' character
-        }
-
-        //Determine whether to add task or dependency by checking if destNode has been changed or not.
-        if (destNode == null) {
-            graph.insertTask(new Task(srcNode, weight));
-        } else {
-            graph.insertDependency(new Dependency(srcNode, destNode, weight));
-        }
     }
 
     /**
