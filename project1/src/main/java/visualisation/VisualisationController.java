2b1bc7f863edd4acc2337d0cc8a30c8340511b8f
Aiden Burgess <aiden9874@gmail.com>
2020-08-26 17:22:11 +1200

    Get missing import to fix merge

--- project1/src/main/java/visualisation/VisualisationController.java
+++ project1/src/main/java/visualisation/VisualisationController.java
@@ -1,5 +1,6 @@
 package main.java.visualisation;
 
+import com.jfoenix.controls.JFXToggleButton;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
