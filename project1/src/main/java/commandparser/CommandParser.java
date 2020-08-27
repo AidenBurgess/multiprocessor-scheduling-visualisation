0376e8a7e98c42830ff579c7d26f31675607b6ae
Charles <cpat430@aucklanduni.ac.nz>
2020-08-28 08:58:50 +1200

    added robustness to commandparser

--- project1/src/main/java/commandparser/CommandParser.java
+++ project1/src/main/java/commandparser/CommandParser.java
@@ -13,76 +13,95 @@ public class CommandParser {
     public static Config parse(String[] args) {
         Config config = new Config();
 
-        try {
-            // if there are no arguments
-            if (args.length == 0) {
-                // prompt user that they can use --help
+        // if there are no arguments
+        if (args.length == 0) {
+            // prompt user that they can use --help
+            String helpMessage = "Too few arguments\nYou can add --help for help with the jar arguments";
+            exitMessage(helpMessage);
+        } else if (args.length == 1) { // when there is just one argument, make sure it is only --help
+            // check if it is --help
+            if (args[0].equals("--help")) {
+                helpMenu();
+            } else {
                 String helpMessage = "Too few arguments\nYou can add --help for help with the jar arguments";
                 exitMessage(helpMessage);
-            } else if (args.length == 1) { // when there is just one argument, make sure it is only --help
-                // check if it is --help
-                if (args[0].equals("--help")) {
-                    helpMenu();
-                } else {
-                    String helpMessage = "Too few arguments\nYou can add --help for help with the jar arguments";
-                    exitMessage(helpMessage);
-                }
-            } else { // if there is --help in the first two, show help
-                if (!args[0].equals("--help") && !args[1].equals("--help")) {
+            }
+        } else { // if there is --help in the first two, show help
+            if (!args[0].equals("--help") && !args[1].equals("--help")) {
 
-                    // get the filename which is the first argument
-                    config.setInputFileName(args[0]);
+                // get the filename which is the first argument
+                config.setInputFileName(args[0]);
 
+                try {
                     // set the number of processors
                     config.setNumProcessors(Integer.parseInt(args[1]));
-
-                    // iterate and find any optional variables.
-                    int i = 2;
-                    while (i < args.length) {
-                        switch (args[i]) {
-                            case "-v":
-                                config.setHasVisualisation(true);
-                                break;
-                            case "-p":
-                                config.setParallelised(true);
-                                config.setNumParallelCores(Integer.parseInt(args[i + 1]));
-                                i++;
-                                break;
-                            case "-o":
-                                config.setOutputFileName(args[i + 1]);
-                                i++;
-                                break;
-                            case "-cv":
-                                // check if the output is valid
-                                config.setCheckValid(true);
-                                break;
-                            case "--help":
-                                helpMenu();
-                            default:
-                                String helpMessage = "Illegal option: You entered '".concat(args[i]).concat("'\nusage: java -jar schedular.jar inputFile.dot P [-o outputFile.dot | -v | -p | --help]");
-                                exitMessage(helpMessage);
-                        }
-                        i++;
-                    }
-
-                    if (config.getOutputFileName() == null) {
-                        config.setDefaultOutputFileName();
-                    }
-
-                } else {
-                    helpMenu();
+                } catch (NumberFormatException e) {
+                    String helpMessage = "The number of processors must be a number.";
+                    exitMessage(helpMessage);
                 }
+
+                // iterate and find any optional variables.
+                int i = 2;
+                while (i < args.length) {
+                    switch (args[i]) {
+                        case "-v":
+                            config.setHasVisualisation(true);
+                            break;
+                        case "-p":
+                            config.setParallelised(true);
+
+                            try {
+                                config.setNumParallelCores(Integer.parseInt(args[i + 1]));
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                String helpMessage = "-p argument must have an argument following for the number of processors.";
+                                exitMessage(helpMessage);
+                            } catch (NumberFormatException e) {
+                                String helpMessage = "The argument following -p must be a number.";
+                                exitMessage(helpMessage);
+                            }
+                            i++;
+                            break;
+                        case "-o":
+
+                            try {
+                                config.setOutputFileName(args[i + 1]);
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                String helpMessage = "-o argument must have an argument following for the file output name.";
+                                exitMessage(helpMessage);
+                            }
+                            i++;
+                            break;
+                        case "-cv":
+                            // check if the output is valid
+                            config.setCheckValid(true);
+                            break;
+                        case "--help":
+                            helpMenu();
+                        default:
+                            String helpMessage = "Illegal option: You entered '".concat(args[i]).concat("'\nusage: java -jar path/to/JarFile.jar inputFile.dot P [-o outputFile.dot | -v | -p | --help]");
+                            exitMessage(helpMessage);
+                    }
+                    i++;
+                }
+
+                if (config.getOutputFileName() == null) {
+                    config.setDefaultOutputFileName();
+                }
+
+            } else {
+                helpMenu();
             }
-        } catch (NumberFormatException e) {
-            String helpMessage = "The input to the number of processors is invalid, please enter a valid number\ne.g java -jar JarFile.jar inputFile.dot 2";
-            exitMessage(helpMessage);
         }
+//        } catch (NumberFormatException e) {
+//            String helpMessage = "The input to the number of processors is invalid, please enter a valid number\ne.g java -jar JarFile.jar inputFile.dot 2";
+//            exitMessage(helpMessage);
+//        }
 
         return config;
     }
 
     private static void helpMenu() {
-        String helpMessage = "Help Menu: \nusage: java -jar JarFile.jar inputFile.dot P [-o outputFile.dot | -v | -p | --help]";
+        String helpMessage = "Help Menu: \nusage: java -jar path/to/JarFile.jar inputFile.dot P [-o outputFile.dot | -v | -p | --help]";
         System.out.println(helpMessage);
         System.exit(1);
     }
