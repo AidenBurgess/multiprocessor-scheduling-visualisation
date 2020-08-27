8599cdbef151b84d8add8752d5edd15422634fdf
Aiden Burgess <aiden9874@gmail.com>
2020-08-27 18:29:57 +1200

    Change text of swich theme toggle to match what clicking it will do

--- project1/src/main/java/visualisation/ThemeSwitcher.java
+++ project1/src/main/java/visualisation/ThemeSwitcher.java
@@ -1,5 +1,7 @@
 package main.java.visualisation;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXToggleButton;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import main.java.visualisation.ganttchart.ScheduleChart;
@@ -7,9 +9,12 @@ import main.java.visualisation.ganttchart.ScheduleChart;
 public class ThemeSwitcher {
     private String _theme = "light";
     private Scene _scene;
+    private JFXToggleButton _switchThemeButton;
 
-    public ThemeSwitcher(Scene scene, String initialCss) {
+    public ThemeSwitcher(Scene scene, JFXToggleButton switchThemeButton, String initialCss) {
         _scene = scene;
+        _switchThemeButton = switchThemeButton;
+        _switchThemeButton.setText("Dark Mode");
         setCss(initialCss);
     }
 
@@ -17,9 +22,11 @@ public class ThemeSwitcher {
         if (_theme.equals("light")) {
             _theme = "dark";
             setCss("css/dark-style.css");
+            _switchThemeButton.setText("Light Mode");
         } else {
             _theme = "light";
             setCss("css/light-style.css");
+            _switchThemeButton.setText("Dark Mode");
         }
     }
 
