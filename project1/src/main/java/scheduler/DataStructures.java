b59b1e244ff48a7ef561ca042fd7fe3314be0d4c
Charles <cpat430@aucklanduni.ac.nz>
2020-08-28 17:50:26 +1200

    commented the datastructures class

--- project1/src/main/java/scheduler/DataStructures.java
+++ project1/src/main/java/scheduler/DataStructures.java
@@ -15,14 +15,26 @@ import java.util.LinkedList;
  * Initialised in VariableScheduler
  */
 public class DataStructures {
+
+    // task graph object with the name, tasks and dependencies of the graph.
     private final TaskGraph _taskGraph;
+
+    // total number of tasks
     private int _numTasks;
-    private ArrayList<ArrayList<Pair<Integer, Integer>>> adjList, revAdjList;
-    private ArrayList<Integer> topologicalOrder, topologicalIndex, taskWeights;
+
+    // adjacency list which shows all node's children and
+    // adjacency list which shows all the children's parents
+    private ArrayList<ArrayList<Pair<Integer, Integer>>> _adjList, _revAdjList;
+
+    // A list containing the topological order, the index of the topological order and the tasks weights
+    private ArrayList<Integer> _topologicalOrder, _topologicalIndex, _taskWeights;
+
+    // map for the task names to their id in the application.
     private HashMap<String, Integer> _taskNameToIdMap;
 
     public DataStructures(TaskGraph taskGraph) {
         _taskGraph = taskGraph;
+
         checkValidWeights();
 
         initIdMapping();
@@ -31,6 +43,9 @@ public class DataStructures {
         initTaskWeights();
     }
 
+    /**
+     * Initialises the tasks to an id.
+     */
     private void initIdMapping() {
         _numTasks = _taskGraph.getTasks().size();
 
@@ -41,9 +56,13 @@ public class DataStructures {
         }
     }
 
+    /**
+     * Checks if the task weights are all positive, and the dependency communication times
+     * are all non-negative.
+     */
     private void checkValidWeights() {
         for (Task task : _taskGraph.getTasks()) {
-            if (task.getTaskTime() <= 0) { // todo check!!!
+            if (task.getTaskTime() <= 0) {
                 throw new SchedulerException("The task weight must be positive.");
             }
         }
@@ -54,13 +73,17 @@ public class DataStructures {
         }
     }
 
+    /**
+     * Initalises the adjacency lists for the tasks from the dependencies.
+     */
     private void initAdjLists() {
         // Instantiating the adjacency list and reverse adjacency list.
-        adjList = new ArrayList<>(_numTasks);
-        revAdjList = new ArrayList<>(_numTasks);
+        _adjList = new ArrayList<>(_numTasks);
+        _revAdjList = new ArrayList<>(_numTasks);
+
         for (int i = 0; i < _numTasks; i++) {
-            adjList.add(new ArrayList<>());
-            revAdjList.add(new ArrayList<>());
+            _adjList.add(new ArrayList<>());
+            _revAdjList.add(new ArrayList<>());
         }
         // Substantiate the adjList and revAdjList.
         // The adjLists use the name -> id mapping from taskNameToIdMap
@@ -68,26 +91,34 @@ public class DataStructures {
         int dependenciesSize = dependencies.size();
         for (int i = 0; i < dependenciesSize; i++) {
             Dependency dependency = dependencies.get(i);
+
             int source = _taskNameToIdMap.get(dependency.getSource());
             int dest = _taskNameToIdMap.get(dependency.getDest());
-            adjList.get(source).add(new Pair<>(dest, dependency.getCommunicationTime()));
-            revAdjList.get(dest).add(new Pair<>(source, dependency.getCommunicationTime()));
+
+            // add the nodes to the adjacency lists.
+            _adjList.get(source).add(new Pair<>(dest, dependency.getCommunicationTime()));
+            _revAdjList.get(dest).add(new Pair<>(source, dependency.getCommunicationTime()));
         }
     }
 
+    /**
+     * Initalises the topological order of the entire graph.
+     */
     private void initTopologicalOrderIndex() {
         // Topological Ordering. The below code finds one valid topological ordering.
         // This also ensures that the TaskGraph input has no cyclic dependencies.
-        topologicalOrder = new ArrayList<>();
+        _topologicalOrder = new ArrayList<>();
 
         // Queue all tasks that have no initial dependencies, and track the inDegree
         boolean[] visited = new boolean[_numTasks];
         int[] inDegree = new int[_numTasks];
         LinkedList<Integer> queue = new LinkedList<>();
+
+        // initialise the queue for all the 0 in degree tasks.
         for (int i = 0; i < _numTasks; i++) {
-            inDegree[i] = revAdjList.get(i).size();
+            inDegree[i] = _revAdjList.get(i).size();
             if (inDegree[i] == 0) {
-                topologicalOrder.add(i);
+                _topologicalOrder.add(i);
                 queue.push(i);
                 visited[i] = true;
             }
@@ -97,14 +128,18 @@ public class DataStructures {
         while (!queue.isEmpty()) {
             int task = queue.poll();
 
-            ArrayList<Pair<Integer, Integer>> revTask = adjList.get(task);
+            ArrayList<Pair<Integer, Integer>> revTask = _adjList.get(task);
             int numRevTasks = revTask.size();
+
             for (int i = 0; i < numRevTasks; i++) {
+
                 Pair<Integer, Integer> dependency = revTask.get(i);
                 int child = dependency.getKey();
                 inDegree[child]--;
+
+                // if the child now has an in degree of 0 and hasn't been visited, then add to queue
                 if (inDegree[child] == 0 && !visited[child]) {
-                    topologicalOrder.add(child);
+                    _topologicalOrder.add(child);
                     visited[child] = true;
                     queue.push(child);
                 }
@@ -112,46 +147,54 @@ public class DataStructures {
         }
 
         // If ind != n, there are some tasks that have dependencies on each other.
-        if (topologicalOrder.size() != _numTasks) {
+        if (_topologicalOrder.size() != _numTasks) {
             throw new SchedulerException("This is not a DAG, hence no valid schedule exists.");
         }
 
-        topologicalIndex = new ArrayList<>();
-        for (int i = 0; i < _numTasks; i++) topologicalIndex.add(0);
-
+        _topologicalIndex = new ArrayList<>();
         for (int i = 0; i < _numTasks; i++) {
-            topologicalIndex.set(topologicalOrder.get(i), i);
+            _topologicalIndex.add(0);
+        }
+
+        // set the id of the task to the corresponding topological order.
+        for (int i = 0; i < _numTasks; i++) {
+            _topologicalIndex.set(_topologicalOrder.get(i), i);
         }
     }
 
+    /**
+     * Initialises the task weights.
+     */
     private void initTaskWeights() {
-        taskWeights = new ArrayList<>();
+
+        _taskWeights = new ArrayList<>();
         for (int i = 0; i < _numTasks; i++) {
-            taskWeights.add(_taskGraph.getTasks().get(i).getTaskTime());
+            _taskWeights.add(_taskGraph.getTasks().get(i).getTaskTime());
         }
     }
 
+    // ------------------ Getters and Setters ------------------- //
     public int getNumTasks() {
         return _numTasks;
     }
 
     public ArrayList<ArrayList<Pair<Integer, Integer>>> getAdjList() {
-        return adjList;
+        return _adjList;
     }
 
     public ArrayList<ArrayList<Pair<Integer, Integer>>> getRevAdjList() {
-        return revAdjList;
+        return _revAdjList;
     }
 
     public ArrayList<Integer> getTopologicalOrder() {
-        return topologicalOrder;
+        return _topologicalOrder;
     }
 
     public ArrayList<Integer> getTopologicalIndex() {
-        return topologicalIndex;
+        return _topologicalIndex;
     }
 
     public ArrayList<Integer> getTaskWeights() {
-        return taskWeights;
+        return _taskWeights;
     }
 }
