8765635bf1445b2be1b8800b651e76a3d2befa6c
Seif Younes <seif99y@gmail.com>
2020-08-28 12:39:40 +1200

    Added Javadoc comments to methods / nested classes and comments on fields

--- project1/src/main/java/visualisation/InformationPoller.java
+++ project1/src/main/java/visualisation/InformationPoller.java
@@ -8,19 +8,34 @@ import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * A class that sets up a timer to poll the various information sources for the visualisation.
+ */
 public class InformationPoller {
 
+    // The timer object that update tasks will be scheduled on
     private Timer _timer;
+    // The display updater that contains the methods for updating/refreshing UI components
     private DisplayUpdater _displayUpdater;
+
+    //The initial delay before beginning the polling cycle
     private long _displayUpdateDelay = 0;
+
+    //The various periods for the polling cycles of different tasks
     private long _schedulesUpdatePeriod = 100;
     private long _cpuRamUpdatePeriod = 500;
     private long _statsUpdatePeriod = 100;
-    private long _currentBound;
+
+    //The objects that are used to retrieve information about the system and scheduler for the UI.
     private SystemPerformanceRetriever _performanceRetriever;
     private InformationHolder _informationHolder = VisualisationDriver.getInformationHolder();
 
-
+    /**
+     * Constructs a poller object with the specified updater and performance retriever. The timer for this poller is
+     * started upon construction.
+     * @param displayUpdater The {@link DisplayUpdater} object used to update/refresh UI components.
+     * @param performanceRetriever The {@link SystemPerformanceRetriever} object used to get RAM and CPU usage info.
+     */
     public InformationPoller(DisplayUpdater displayUpdater, SystemPerformanceRetriever performanceRetriever) {
         _displayUpdater = displayUpdater;
         _performanceRetriever = performanceRetriever;
@@ -28,9 +43,8 @@ public class InformationPoller {
     }
 
     /**
-     * starts the timer which runs scheduled tasks with a delay and a period
-     * - Delay: initial delay before a task is first performed
-     * - Period: delay period between two occurences of the same task
+     * Starts the poller's {@link Timer} which runs scheduled tasks with a delay and periods. The delays/periods are
+     * constant and remain unchanged throughout execution.
      */
     private void startTimer() {
 
@@ -43,7 +57,10 @@ public class InformationPoller {
 
     }
 
-
+    /**
+     * A task to be scheduled on the {@link InformationPoller}'s timer. Its job is to update the current and best schedules on
+     * the UI with information retrieved from the {@link InformationHolder}.
+     */
     private class ScheduleUpdateTask extends TimerTask {
 
         @Override
@@ -63,7 +80,10 @@ public class InformationPoller {
         }
     }
 
-
+    /**
+     * A task to be scheduled on the {@link InformationPoller}'s timer. Its job is to update the CPU usage graph and the RAM
+     * usage graph with information retrieved from the {@link SystemPerformanceRetriever}.
+     */
     private class GraphUpdateTask extends TimerTask {
 
         @Override
@@ -76,7 +96,10 @@ public class InformationPoller {
         }
     }
 
-
+    /**
+     * A task to be scheduled on the {@link InformationPoller}'s timer. Its job is to update the displayed statistics on the UI
+     * with information retrieved from the {@link InformationHolder}
+     */
     private class StatsUpdateTask extends TimerTask {
 
         @Override
